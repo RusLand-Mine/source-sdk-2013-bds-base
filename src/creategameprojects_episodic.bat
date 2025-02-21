324e296fbd6aecaf3371689b4c0c061179bb9e3b
Bitl <bitlblox@gmail.com>
2025-02-21 15:39:12 -0700

    fixed batch files

--- src/creategameprojects_episodic.bat
+++ src/creategameprojects_episodic.bat
@@ -1,3 +1,3 @@
 @echo off
-devtools\bin\vpc.exe /episodic /define:SOURCESDK /define:BDSBASE +game /mksln game_hl2mp.sln
+devtools\bin\vpc.exe /episodic /define:SOURCESDK /define:BDSBASE +game /mksln game_episodic.sln
 pause
