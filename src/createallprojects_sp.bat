d6d0eb9e6862a0a549bf58caf99903b4298f7052
Bitl <bitlblox@gmail.com>
2025-02-22 12:21:26 -0700

    add singleplayer files. merge lost coast stuff into hl2 binaries as the files used are almost the same.

--- src/createallprojects_sp.bat
+++ src/createallprojects_sp.bat
@@ -1,3 +1,3 @@
 @echo off
-devtools\bin\vpc.exe /hl2 /episodic /lostcoast /define:SOURCESDK /define:BDSBASE +everything /mksln everything_sp.sln
+devtools\bin\vpc.exe /hl2 /episodic /define:SOURCESDK /define:BDSBASE +everything /mksln everything_sp.sln
 pause
--- src/creategameprojects_lostcoast.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-devtools\bin\vpc.exe /lostcoast /define:SOURCESDK /define:BDSBASE +game /mksln game_lostcoast.sln
-pause
